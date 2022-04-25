3b85cbaf9a9031036b66062b6766e28651787889
Michael Truong <truong.minh.michael@gmail.com>
2022-04-25 13:44:21 -0700

    Added state function to check if valid paranthesis

--- CalculatorPt2.java
+++ CalculatorPt2.java
@@ -45,12 +45,27 @@ public class CalculatorPt2 {
     private static boolean startState(String str) {
         if (str.isEmpty())
             return false;
+        if (!isParanthesisValid(str))
+            return false; 
         char num = str.charAt(0);
         if (num_alphabet.contains(num) || num == '.' || num == '(') {
             //System.out.println("huh");
             return findSubStr(str);
         }
         return false;
+    }
+
+    public static boolean isParanthesisValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+            if(a == '(' || a == '[' || a == '{') stack.push(a);
+            else if(stack.empty()) return false;
+            else if(a == ')' && stack.pop() != '(') return false;
+            else if(a == ']' && stack.pop() != '[') return false;
+            else if(a == '}' && stack.pop() != '{') return false;
+        }
+        return stack.empty();
     }
 
     private static boolean findSubStr(String str) {
